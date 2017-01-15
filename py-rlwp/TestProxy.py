9273421da83640c45a6157b56d0ae148ccc9d5a6
johnnyczhong <johnnyczhong@gmail.com>
2017-01-15 05:52:55 +0000

    daemon-ized the psuedoproxy. ready to implement it for my application.

--- py-rlwp/TestProxy.py
+++ py-rlwp/TestProxy.py
@@ -14,6 +14,8 @@
 from twisted.internet import reactor
 from twisted.internet.protocol import ServerFactory
 from twisted.protocols.basic import NetstringReceiver
+from twisted.application import internet, service
+from twisted.python import log
 import requests
 import time
 import json
@@ -23,17 +25,22 @@ RLC_HEADER = 'X-Rate-Limit-Count'
 RATE_PER_10 = 10
 RATE_PER_600 = 500
 
+PORT = 8001
+IFACE = 'localhost'
+
+
 # purpose: performs the action desired
 # in this case, makes an https request on the client's behalf.
 # low context, performs simple operations to be determined by factory
-class RequestService():
+class RequestService(service.Service):
+    
     # purpose: make request to Riot's API
     # return: web response
     def make_request(self, url): 
         url = url.decode('utf-8')
-        #response = requests.get(url)
-        #return response
-        return url
+        log.msg('request url: {}'.format(url))
+        response = requests.get(url)
+        return response.json()
     
     # TODO: extract status code from response
     def get_response_codes(self, response):
@@ -46,10 +53,11 @@ class RequestService():
             msg = 'Retry'
         else:
             msg = 'Unknown Status Code'
-        print('msg: {}'.format(msg))
+        log.msg('msg: {}'.format(msg))
         return msg
 
     # TODO: extract rate limit header
+    # returns: [int(num_calls_10s), int(num_calls_600s)]
     def parse_rate_limit(self, response):
         num_calls = []
         split1 = []
@@ -80,14 +88,16 @@ class RequestProtocol(NetstringReceiver):
             response = json.dumps(response) # encode as JSON string
             response = response.encode('utf-8') # encode JSON string as bytes object
 
+            log.msg('returning {} bytes to calling application'.format(len(response)))
             self.sendString(response) # send response to client as bytes/netstring
             self.transport.loseConnection()
         else: # attempt function again
+            # send back a response that would tell the calling application
+            # an expected timeout
             #self.sendString(timeout)
             wait_time = (self.factory.numProtocols 
                     - self.factory.curr_tokens_10) * (10/self.factory.tokens_per_10)
-            print('waiting for {}'.format(wait_time))
-            reactor.callLater(self.factory.numProtocols, 
+            reactor.callLater(0,
                     self.process_request, request)
 
     def connectionLost(self, reason):
@@ -103,56 +113,52 @@ class RequestFactory(ServerFactory):
     def __init__(self, service):
         self.service = service
         self.numProtocols = 0
-        self.curr_tokens_10 = 10
+        self.curr_tokens_10 = 1
         self.last_token_time = time.time() # now
-        #self.tokens_10_minimum_refresh = 1
         self.tokens_per_10 = 10 # num requests allowed per 10s
 
-
+    
+    # move this to protocol?
     def req(self, url):
-        # TODO: rate limit check here
-        # this version of the check freezes due to an infinite loop
-        # have to reset the counter somehow.
-        # drip rate/burst rate?
-        """
-        if self.rate_limit_calls_per_10 >= (RATE_PER_10-1): # 9/10
-            time.sleep(1) # stop for 1s
-            return self.req(url) # try again
-        elif self.rate_limit_calls_per_600 >= (RATE_PER_600 - 50): #350/500
-            time.sleep(30) # stop for 30s
-            return self.req(url) # try again
-        """
-        # check how many tokens we have, replenish if possible
-        # if the current time is after minimum drip time
-        # drip the appropriate number of tokens
-        # if 1 second has elapsed since last token was distributed
+        # TODO: hybrid rate limit check between Riot Api and own rate limiter
         now = time.time()
-        #print(self.last_token_time)
-        if now >= 1 + self.last_token_time:
-            #tokens = int(self.tokens_per_10 * ((time.time() - self.last_token_time)/10))
-            # num of tokens dripped = seconds passed since last tokens dripped
-            # not to exceed 10
-            tokens = int(time.time() - self.last_token_time)
-            tokens = 10 if (tokens > 10) else tokens # redundant?
-            #print('New token count: {} at {}'.format(tokens, now))
-            self.curr_tokens_10 += tokens
-            self.curr_tokens_10 = 10 if (self.curr_tokens_10 > 10) else self.curr_tokens_10
+
+        # refresh timer of 1.2s. restore token count and update last token distribution time
+        if now >= (1.2 + self.last_token_time):
             self.last_token_time = now
-        # if we don't have tokens, have reactor come back at next token refresh time.
-        if self.curr_tokens_10 <= 1:
-            #reactor.callLater(1, self.req, url)
-            return None
-        else:
-            #print('Subtracting a token from: {}'.format(self.curr_tokens_10))
-            self.curr_tokens_10 -= 1
-            #print('Current token count: {}'.format(self.curr_tokens_10))
             try:
                 resp = self.service.make_request(url) # make request to Riot
+                #resp = self.service.startService(url)
                 return resp
             except:
                 return None # request failed
+        else:
+            return None
+
+
+# === TAC FILE STARTS HERE ===
+# start services
+top_service = service.MultiService()
+proxy_service = RequestService()
+
+# init tcp service to listen to port
+# this uses the factory I've created to process incoming requests
+factory = RequestFactory(proxy_service)
+tcp_service = internet.TCPServer(PORT, factory, interface = IFACE)
+tcp_service.setServiceParent(top_service)
+
+# assign application variable and set services
+application = service.Application('TestProxy')
+top_service.setServiceParent(application)
+
+
+# === TAC FILE STOPS HERE === 
+
+# testing
 def main():
     service = RequestService()
+    main_service = service.MultiService()
+
     factory = RequestFactory(service)
     reactor.listenTCP(8001, factory)
     reactor.run()
